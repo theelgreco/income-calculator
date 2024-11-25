550813bffe53409e09521735d76db46854d2832b
theelgreco <stelios.kakoliris@yahoo.com>
2024-11-25 20:36:25 +0000

    testing deploy hook

--- backend/src/firebase/firebase.ts
+++ backend/src/firebase/firebase.ts
@@ -1,10 +1,5 @@
-// Import the functions you need from the SDKs you need
 import { FirebaseApp, initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyCIMviedkMP5RgWGnC0dRuzKTiPtfuJw9M",
     authDomain: "income-tracker-88b7c.firebaseapp.com",
@@ -15,5 +10,4 @@ const firebaseConfig = {
     measurementId: "G-3JFPC1J83G",
 };
 
-// Initialize Firebase
 export const app: FirebaseApp = initializeApp(firebaseConfig);
