7ec1a8eb50afa2332549f7bb3ace12433306405f
theelgreco <stelios.kakoliris@yahoo.com>
2025-09-18 14:04:17 +0100

    adds userStore

---
+++ frontend/src/stores/userStore.ts
@@ -0,0 +1,59 @@
+import { AuthenticationServer } from "@/api/auth";
+import { AuthenticationApi, UsersApi, type User } from "@/api/generated";
+import { defaultApiConfiguration } from "@/fetch";
+import router from "@/router";
+import { defineStore } from "pinia";
+import { ref } from "vue";
+
+export const useUserStore = defineStore("user", () => {
+    const user = ref<User | null>(null);
+
+    const authServer = new AuthenticationServer();
+
+    const authenticationApi = new AuthenticationApi(defaultApiConfiguration);
+
+    const usersApi = new UsersApi(defaultApiConfiguration);
+
+    async function validateJWT() {
+        await authenticationApi.apiValidateJWTGet();
+
+        if (!user.value) {
+            user.value = await usersApi.apiUserGet();
+        }
+    }
+
+    async function login(email_or_username: string, password: string) {
+        if (email_or_username && password) {
+            try {
+                const { jwt } = await authServer.login({ email_or_username, password });
+
+                localStorage.setItem("jwt", jwt);
+
+                user.value = await usersApi.apiUserGet();
+
+                router.replace({ name: "year", params: { year: new Date().getFullYear() } });
+            } catch (err: any) {
+                console.error(err);
+            }
+        }
+    }
+
+    async function signUp(username: string, email: string, password: string) {
+        if (username && email && password) {
+            try {
+                await authServer.signUp({ email: email, password: password, username: username });
+                await login(email, password);
+            } catch (err: any) {
+                console.error(err);
+            }
+        }
+    }
+
+    function logout() {
+        localStorage.removeItem("jwt");
+        user.value = null;
+        router.replace({ name: "login" });
+    }
+
+    return { user, login, signUp, logout, validateJWT };
+});
