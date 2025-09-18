3e180594bf7d5d32c78dd17c6ce9ea3fcc703557
theelgreco <stelios.kakoliris@yahoo.com>
2025-09-18 16:46:07 +0100

    updates icons

--- frontend/src/views/TransactionsView.vue
+++ frontend/src/views/TransactionsView.vue
@@ -3,12 +3,14 @@ import { type Transaction, TransactionsApi } from "@/api/generated";
 import { defaultApiConfiguration } from "@/fetch";
 import SvgIcon from "@jamescoyle/vue-icon";
 import { onMounted, ref } from "vue";
-import { mdiCardsOutline, mdiPencilOutline, mdiTrashCanOutline } from "@mdi/js";
+import { mdiCardsOutline, mdiPencilOutline } from "@mdi/js";
 import { toast } from "vue-sonner";
 import ConfirmTransactionDeleteDialog from "@/components/dialogs/ConfirmTransactionDeleteDialog.vue";
 import Tooltip from "@/components/ui/tooltip/Tooltip.vue";
 import TooltipTrigger from "@/components/ui/tooltip/TooltipTrigger.vue";
 import TooltipContent from "@/components/ui/tooltip/TooltipContent.vue";
+import Button from "@/components/ui/button/Button.vue";
+import { Trash } from "lucide-vue-next";
 
 const transactionsApi = new TransactionsApi(defaultApiConfiguration);
 
@@ -66,33 +68,34 @@ onMounted(() => {
                 <p class="text-xl font-medium">{{ transaction.name }}</p>
                 <div class="flex items-center gap-3">
                     <p class="text-xl mr-1">£{{ parseFloat(transaction.amountInPence).toFixed(2) }}</p>
-                    <div class="flex gap-2">
+                    <div class="flex">
                         <Tooltip>
                             <TooltipContent> Edit {{ transaction.name }} </TooltipContent>
                             <TooltipTrigger asChild>
-                                <RouterLink :to="{ name: 'editTransaction', params: { transaction: transaction.id } }">
-                                    <SvgIcon
-                                        type="mdi"
-                                        :path="mdiPencilOutline"
-                                        class="text-gray-400 hover:text-gray-600 cursor-pointer"
-                                        :size="18"
-                                    />
-                                </RouterLink>
+                                <Button asChild variant="ghost">
+                                    <RouterLink :to="{ name: 'editTransaction', params: { transaction: transaction.id } }">
+                                        <SvgIcon
+                                            type="mdi"
+                                            :path="mdiPencilOutline"
+                                            class="text-gray-400 hover:text-gray-600 cursor-pointer"
+                                            :size="18"
+                                        />
+                                    </RouterLink>
+                                </Button>
                             </TooltipTrigger>
                         </Tooltip>
                         <Tooltip>
                             <TooltipContent> Delete {{ transaction.name }} </TooltipContent>
                             <TooltipTrigger asChild>
-                                <SvgIcon
-                                    type="mdi"
-                                    :path="mdiTrashCanOutline"
-                                    class="text-gray-400 hover:text-gray-600 cursor-pointer"
-                                    :size="18"
+                                <Button
+                                    variant="ghost"
                                     @click="
                                         selectedTransactionId = transaction.id;
                                         deleteConfirmDialog.open = true;
                                     "
-                                />
+                                >
+                                    <Trash class="text-gray-400 hover:text-gray-600 cursor-pointer" :size="18" />
+                                </Button>
                             </TooltipTrigger>
                         </Tooltip>
                     </div>
