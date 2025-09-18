c0e5ed204c405f21b05c0f45edc7888868496a24
theelgreco <stelios.kakoliris@yahoo.com>
2025-09-18 14:05:07 +0100

    replaces primevue drawer with shadcn sidebar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,14 +3,19 @@ import Nav from "./components/Nav.vue";
 import { RouterView, useRoute } from "vue-router";
 import { Toaster } from "./components/ui/sonner";
 import "vue-sonner/style.css";
+import SidebarProvider from "./components/ui/sidebar/SidebarProvider.vue";
+import MobileSidebar from "./components/MobileSidebar.vue";
 
 const route = useRoute();
 </script>
 
 <template>
     <Toaster position="top-right" />
-    <main class="w-full h-full flex flex-col gap-5 overscroll-none">
-        <Nav v-if="route.meta.showNav" />
-        <RouterView />
-    </main>
+    <SidebarProvider>
+        <MobileSidebar />
+        <main class="w-full h-full flex flex-col gap-5 overscroll-none">
+            <Nav v-if="route.meta.showNav" />
+            <RouterView />
+        </main>
+    </SidebarProvider>
 </template>
--- frontend/src/components/Nav.vue
+++ frontend/src/components/Nav.vue
@@ -1,31 +1,10 @@
 <script setup lang="ts">
-import { mdiCardsOutline, mdiClose, mdiCogOutline, mdiFinance, mdiHomeOutline, mdiMenu, mdiPlusOutline } from "@mdi/js";
-import { ref, watch } from "vue";
+import { mdiCardsOutline, mdiFinance, mdiMenu, mdiPlusOutline } from "@mdi/js";
 import Button from "./ui/button/Button.vue";
 import SvgIcon from "@jamescoyle/vue-icon";
 import ProfileAvatar from "./ProfileAvatar.vue";
-import Drawer from "primevue/drawer";
-import { RouterLink, useRoute } from "vue-router";
-import { useWindowSize } from "@vueuse/core";
-
-const route = useRoute();
-
-const drawerVisible = ref(false);
-
-const { width: windowWidth } = useWindowSize();
-
-watch(
-    () => route.path,
-    () => {
-        drawerVisible.value = false;
-    }
-);
-
-watch(windowWidth, () => {
-    if (windowWidth.value > 767) {
-        drawerVisible.value = false;
-    }
-});
+import { RouterLink } from "vue-router";
+import SidebarTrigger from "./ui/sidebar/SidebarTrigger.vue";
 </script>
 
 <template>
@@ -49,64 +28,10 @@ watch(windowWidth, () => {
                     <SvgIcon type="mdi" :path="mdiPlusOutline" :size="16" />
                 </RouterLink>
             </Button>
-            <ProfileAvatar />
-            <SvgIcon type="mdi" :path="mdiMenu" class="cursor-pointer hover:bg-grays-light-100 md:hidden" @click="drawerVisible = true" />
+            <ProfileAvatar class="max-md:hidden" />
+            <SidebarTrigger class="cursor-pointer md:hidden">
+                <SvgIcon type="mdi" :path="mdiMenu" />
+            </SidebarTrigger>
         </div>
     </nav>
-    <Drawer
-        v-if="windowWidth < 768"
-        v-model:visible="drawerVisible"
-        position="right"
-        style="max-width: 100vw; padding: 1rem; gap: 0.25rem"
-        blockScroll
-    >
-        <template #container="{ closeCallback }">
-            <div class="flex justify-between">
-                <RouterLink
-                    :to="{ name: 'year', params: { year: new Date().getFullYear() } }"
-                    class="flex items-center gap-2 select-none cursor-pointer"
-                >
-                    <SvgIcon type="mdi" :path="mdiFinance" :size="34" />
-                    <h2 class="font-black">Income Tracker</h2>
-                </RouterLink>
-                <Button variant="ghost" @click="closeCallback" class="-mr-2">
-                    <SvgIcon type="mdi" :path="mdiClose" />
-                </Button>
-            </div>
-            <div class="flex flex-col py-3">
-                <RouterLink
-                    :to="{ name: 'year', params: { year: new Date().getFullYear() } }"
-                    class="flex items-center gap-2 hover:bg-grays-light-100 p-3 rounded-sm transition-all select-none"
-                >
-                    <SvgIcon type="mdi" :path="mdiHomeOutline" :size="20" class="text-grays-light-500" />
-                    <p>Dashboard</p>
-                </RouterLink>
-                <RouterLink
-                    :to="{ name: 'transactions' }"
-                    class="flex items-center gap-2 hover:bg-grays-light-100 p-3 rounded-sm transition-all select-none"
-                >
-                    <SvgIcon type="mdi" :path="mdiCardsOutline" :size="20" class="text-grays-light-500" />
-                    <p>Transactions</p>
-                </RouterLink>
-                <RouterLink
-                    :to="{ name: 'createTransaction' }"
-                    class="flex items-center gap-2 hover:bg-grays-light-100 p-3 rounded-sm transition-all select-none"
-                >
-                    <SvgIcon type="mdi" :path="mdiPlusOutline" :size="20" class="text-grays-light-500" />
-                    <p>Add transaction</p>
-                </RouterLink>
-                <RouterLink
-                    :to="{ name: 'year', params: { year: new Date().getFullYear() } }"
-                    class="flex items-center gap-2 hover:bg-grays-light-100 p-3 rounded-sm transition-all select-none"
-                >
-                    <SvgIcon type="mdi" :path="mdiCogOutline" :size="20" class="text-grays-light-500" />
-                    <p>Settings</p>
-                </RouterLink>
-            </div>
-            <div class="flex flex-col gap-6 mt-auto">
-                <hr />
-                <ProfileAvatar :isInDrawer="true" />
-            </div>
-        </template>
-    </Drawer>
 </template>
