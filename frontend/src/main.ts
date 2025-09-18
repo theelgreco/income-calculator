0f4fdf9a48204e8e456d3d1733d6ce66042431dc
theelgreco <stelios.kakoliris@yahoo.com>
2025-09-18 14:04:01 +0100

    adds pinia

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,23 +3,22 @@ import "@/assets/styles/main.css";
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import { createPinia } from "pinia";
 import Primevue from "primevue/config";
 //@ts-ignore
 import Aura from "@/presets/aura";
 import Tooltip from "primevue/tooltip";
-import ConfirmationService from "primevue/confirmationservice";
 
+const pinia = createPinia();
 const app = createApp(App);
-
-app.use(router);
 
 app.use(Primevue, {
     unstyled: true,
     pt: Aura,
 });
 
-app.use(ConfirmationService);
-
 app.directive("tooltip", Tooltip);
 
+app.use(pinia);
+app.use(router);
 app.mount("#app");
