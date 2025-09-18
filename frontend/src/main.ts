f06b47d34527e896e6bd4b553e2a76dfa932d7db
theelgreco <stelios.kakoliris@yahoo.com>
2025-09-18 16:24:34 +0100

    removes primevue

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,23 +1,11 @@
 import "@/assets/styles/main.css";
-
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import { createPinia } from "pinia";
-import Primevue from "primevue/config";
-//@ts-ignore
-import Aura from "@/presets/aura";
-import Tooltip from "primevue/tooltip";
 
 const pinia = createPinia();
 const app = createApp(App);
-
-app.use(Primevue, {
-    unstyled: true,
-    pt: Aura,
-});
-
-app.directive("tooltip", Tooltip);
 
 app.use(pinia);
 app.use(router);
--- frontend/src/presets/aura/tabpanels/index.js
+++
@@ -1,3 +0,0 @@
-export default {
-    root: "bg-white dark:bg-white-900 text-surface-900 dark:text-surface-0/80 outline-0 p-4.5 pt-3.5",
-};
