48f220e443d5df6d07402a97645690ee3b9b7b3d
theelgreco <stelios.kakoliris@yahoo.com>
2025-09-25 00:22:20 +0100

    adds disabled prop to login form buttons

--- frontend/src/components/Login/LoginForm.vue
+++ frontend/src/components/Login/LoginForm.vue
@@ -5,7 +5,7 @@ import { Lock, Mail } from "lucide-vue-next";
 import TextDivider from "@/components/TextDivider.vue";
 import z from "zod";
 import useForm from "@/composables/useForm";
-import { Form, FormFieldGroup, FormField, FormFieldInput, FormFieldIcon, FormFieldError, FormFooter } from "@/components/form";
+import { Form, FormFieldGroup, FormField, FormFieldInput, FormFieldIcon, FormFieldError, FormFooter, FormError } from "@/components/form";
 
 const { signUp, login, googleRedirect } = useUserStore();
 
@@ -26,12 +26,8 @@ const handleSignUp = form.submit(async () => {
 </script>
 
 <template>
-    <Form
-        :formSchema="formSchema"
-        :form="form"
-        class="flex flex-col gap-4 p-15 my-auto md:shadow-xl bg-white rounded-xl"
-        @submit.prevent=""
-    >
+    <Form :formSchema="formSchema" :form="form" class="flex flex-col gap-4 p-15 md:shadow-xl bg-white rounded-xl" @submit.prevent="">
+        <FormError class="mb-5" />
         <FormFieldGroup field="email">
             <FormField>
                 <FormFieldInput placeholder="Email" />
@@ -46,19 +42,21 @@ const handleSignUp = form.submit(async () => {
             </FormField>
             <FormFieldError />
         </FormFieldGroup>
-        <FormFooter>
+        <FormFooter class="flex flex-col gap-6">
             <div class="flex flex-col mt-4 gap-3">
-                <Button class="rounded-xs py-5" @click="handleLogin" type="submit">Login</Button>
-                <Button variant="outline" class="rounded-xs py-5" @click="handleSignUp" type="submit">Sign up</Button>
+                <Button class="rounded-xs py-5" @click="handleLogin" type="submit" :disabled="form.submitting.value">Login</Button>
+                <Button variant="outline" class="rounded-xs py-5" @click="handleSignUp" type="submit" :disabled="form.submitting.value"
+                    >Sign up</Button
+                >
             </div>
             <TextDivider>or</TextDivider>
-            <Button variant="outline" class="relative rounded-xs py-5" @click="googleRedirect">
+            <Button variant="outline" class="relative rounded-xs py-5" @click="googleRedirect" :disabled="form.submitting.value">
                 <img class="absolute left-2" src="@/assets/icons/google.svg" width="20" />
                 Sign in with Google
             </Button>
-            <div class="flex flex-col gap-3 mt-20">
+            <div class="flex flex-col gap-3 mt-10">
                 <small class="text-center">Want to try it out first?</small>
-                <Button variant="secondary" size="lg">Continue as guest</Button>
+                <Button variant="secondary" size="lg" :disabled="form.submitting.value">Continue as guest</Button>
             </div>
         </FormFooter>
     </Form>
