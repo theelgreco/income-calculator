dd46963c0c280d6dbf9ff4f46046013a5c773f93
theelgreco <stelios.kakoliris@yahoo.com>
2025-09-25 00:21:50 +0100

    styling fixes

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import Button from "@/components/ui/button/Button.vue";
 import { onMounted } from "vue";
 import { useUserStore } from "@/stores/userStore";
 import { useRoute } from "vue-router";
@@ -20,10 +19,19 @@ onMounted(() => {
 </script>
 
 <template>
-    <div class="h-screen flex bg-gradient-to-tl from-yellow-50 to-white to-40%">
-        <div class="flex max-lg:hidden w-1/2 border my-60 mx-20"></div>
+    <div class="h-screen flex bg-gradient-to-tr from-yellow-50 to-white to-40%">
+        <div class="flex flex-col gap-5 max-lg:hidden w-1/2 my-60 mx-20">
+            <div class="flex flex-wrap items-center gap-5">
+                <img class="bg-white border rounded-full p-5 shadow-lg w-[100px] h-[100px]" src="/192w/icon.png" />
+                <div>
+                    <h1 class="font-black text-3xl">Take control of your finances today</h1>
+                    <h2 class="text-lg font-light">One <i class="font-bold">simple</i> transaction at a time</h2>
+                </div>
+            </div>
+            <!-- <div class="w-full border h-full"></div> -->
+        </div>
         <div
-            class="md:bg-[#F8F8F8] md:border md:shadow-2xl flex flex-col gap-15 w-full lg:w-1/2 sm:px-10 md:px-35 rounded-3xl md:min-w-[700px]"
+            class="md:bg-[#F8F8F8] md:border md:shadow-2xl flex flex-col justify-center w-full lg:w-1/2 sm:px-10 md:px-35 rounded-3xl md:min-w-[700px]"
         >
             <LoginForm />
         </div>
