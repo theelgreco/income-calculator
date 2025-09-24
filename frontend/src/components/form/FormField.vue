4c0e4051b61f8bec1c911878e7bc58585d391ae9
theelgreco <stelios.kakoliris@yahoo.com>
2025-09-25 00:21:39 +0100

    adds submission loading

--- frontend/src/components/form/FormField.vue
+++ frontend/src/components/form/FormField.vue
@@ -1,5 +1,21 @@
+<script setup lang="ts">
+import { inject } from "vue";
+import { motion } from "motion-v";
+import { LoaderCircle } from "lucide-vue-next";
+import type { FormContext } from "./Form.vue";
+
+const form = inject<FormContext>("formContext");
+</script>
+
 <template>
     <div class="group relative w-full items-center">
         <slot />
+        <motion.div
+            v-if="form?.submitting.value"
+            class="absolute end-0 inset-y-0 flex items-center justify-center px-3"
+            :animate="{ rotate: 360, transition: { repeat: Infinity, duration: 1 } }"
+        >
+            <LoaderCircle />
+        </motion.div>
     </div>
 </template>
--- frontend/src/composables/useForm.ts
+++ frontend/src/composables/useForm.ts
@@ -1,3 +1,4 @@
+import { getErrorMessage } from "@/lib/utils";
 import { ref } from "vue";
 import z from "zod";
 
@@ -14,27 +15,37 @@ export default function useForm<FormSchemaType>(formSchema: z.ZodObject) {
     /** Ref object holding the form’s current state. */
     const form = ref<FormSchemaType>(formSchema.default({}).parse({}) as FormSchemaType);
 
+    const error = ref<string>();
+
     /** Ref object holding validation errors returned by Zod. */
     const errors = ref<z.core.$ZodFlattenedError<FormSchemaType>>();
 
     /** The form's initial state (used when resetting). */
     const initialState = JSON.parse(JSON.stringify(form.value));
 
+    const submitting = ref(false);
+
     /**
      * Validates the form before running the given callback.
      * - If valid → runs the callback.
      * - If invalid → populates `errors`.
      */
-    const submit = (callback: () => any): (() => void) => {
+    const submit = (callback: () => Promise<any>): (() => void) => {
         return async () => {
             try {
+                resetErrors();
+                submitting.value = true;
                 formSchema.parse(form.value);
                 await callback();
             } catch (err: unknown) {
                 if (err instanceof z.ZodError) {
                     const typedErr = err as z.ZodError<FormSchemaType>;
                     errors.value = typedErr.flatten();
+                } else {
+                    error.value = getErrorMessage(err);
                 }
+            } finally {
+                submitting.value = false;
             }
         };
     };
@@ -48,6 +59,7 @@ export default function useForm<FormSchemaType>(formSchema: z.ZodObject) {
     /** Clears all validation errors without affecting form values. */
     const resetErrors = (): void => {
         errors.value = undefined;
+        error.value = undefined;
     };
 
     /** Removes validation errors for a specific field. */
@@ -74,5 +86,5 @@ export default function useForm<FormSchemaType>(formSchema: z.ZodObject) {
         return !!errors.value?.fieldErrors?.[field]?.length;
     };
 
-    return { form, errors, submit, resetForm, resetErrors, resetFieldError, setFieldError, fieldHasError };
+    return { form, errors, error, submitting, submit, resetForm, resetErrors, resetFieldError, setFieldError, fieldHasError };
 }
