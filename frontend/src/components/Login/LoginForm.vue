9405ea3d3379713672c894268d19164e2e14a4c0
theelgreco <stelios.kakoliris@yahoo.com>
2025-09-24 01:06:22 +0100

    moves login form to new component

---
+++ frontend/src/components/Login/LoginForm.vue
@@ -0,0 +1,57 @@
+<script setup lang="ts">
+import Button from "@/components/ui/button/Button.vue";
+import { useUserStore } from "@/stores/userStore";
+import { Lock, Mail } from "lucide-vue-next";
+import TextDivider from "@/components/TextDivider.vue";
+import z from "zod";
+import useForm from "@/composables/useForm";
+import { Form, FormHeader, FormFieldGroup, FormField, FormFieldInput, FormFieldIcon, FormFieldError, FormFooter } from "@/components/form";
+
+const { signUp, login, googleRedirect } = useUserStore();
+
+const formSchema = z.object({
+    email: z.email().default(""),
+    password: z.string().min(8, "Minimum 8 characters").default(""),
+});
+
+const form = useForm<z.infer<typeof formSchema>>(formSchema);
+
+const handleLogin = form.submit(() => {
+    login(form.form.value.email, form.form.value.password);
+});
+
+const handleSignUp = form.submit(() => {
+    signUp(form.form.value.email, form.form.value.password);
+});
+</script>
+
+<template>
+    <Form :formSchema="formSchema" :form="form" class="flex flex-col gap-4 flex-grow p-15 shadow-xl bg-white rounded-xl">
+        <FormHeader></FormHeader>
+        <FormFieldGroup field="email">
+            <FormField>
+                <FormFieldInput placeholder="Email" />
+                <FormFieldIcon :icon="Mail" />
+            </FormField>
+            <FormFieldError />
+        </FormFieldGroup>
+        <FormFieldGroup field="password">
+            <FormField>
+                <FormFieldInput type="password" placeholder="Password" />
+                <FormFieldIcon :icon="Lock" />
+            </FormField>
+            <FormFieldError />
+        </FormFieldGroup>
+        <FormFooter>
+            <div class="flex flex-col mt-4 gap-3">
+                <Button class="rounded-xs py-5" @click="handleLogin" type="submit">Login</Button>
+                <Button variant="outline" class="rounded-xs py-5" @click="handleSignUp" type="submit">Sign up</Button>
+            </div>
+            <TextDivider>or</TextDivider>
+            <Button variant="outline" class="relative rounded-xs py-5" @click="googleRedirect">
+                <img class="absolute left-2" src="@/assets/icons/google.svg" width="20" />
+                Sign in with Google
+            </Button>
+        </FormFooter>
+    </Form>
+</template>
--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -1,47 +1,13 @@
 <script setup lang="ts">
 import Button from "@/components/ui/button/Button.vue";
-import Input from "@/components/ui/input/Input.vue";
-import { onMounted, ref, watch } from "vue";
+import { onMounted } from "vue";
 import { useUserStore } from "@/stores/userStore";
-import { Lock, Mail } from "lucide-vue-next";
-import TextDivider from "@/components/TextDivider.vue";
 import { useRoute } from "vue-router";
-
-enum TabChoices {
-    LOGIN,
-    SIGN_UP,
-}
+import LoginForm from "@/components/login/LoginForm.vue";
 
 const route = useRoute();
 
-const { signUp, login, signInWithGoogle, googleRedirect } = useUserStore();
-
-const email = ref<string>();
-
-const password = ref<string>();
-
-const activeTab = ref<TabChoices>(TabChoices.LOGIN);
-
-function handleLogin() {
-    if (email.value && password.value) {
-        login(email.value, password.value);
-    }
-}
-
-function handleSignUp() {
-    if (email.value && password.value) {
-        signUp(email.value, password.value);
-    }
-}
-
-function clearForm() {
-    email.value = undefined;
-    password.value = undefined;
-}
-
-watch(activeTab, () => {
-    clearForm();
-});
+const { signInWithGoogle } = useUserStore();
 
 onMounted(() => {
     const urlParams = new URLSearchParams(route.hash.replace("#", ""));
@@ -57,30 +23,7 @@ onMounted(() => {
     <div class="h-screen flex bg-gradient-to-tl from-yellow-50 to-white to-40%">
         <div class="flex max-lg:hidden w-1/2 border my-60 mx-20"></div>
         <div class="bg-[#F8F8F8] shadow-2xl flex flex-col gap-15 w-full lg:w-1/2 my-30 py-25 px-35 border rounded-3xl">
-            <!-- Form Container -->
-            <div class="flex flex-col gap-4 flex-grow p-15 shadow-xl bg-white rounded-xl">
-                <div class="relative w-full items-center">
-                    <Input v-model="email" type="email" placeholder="Email" class="pl-10 py-5 rounded-xs" />
-                    <span class="absolute start-0 inset-y-0 flex items-center justify-center px-3">
-                        <Mail class="size-4 text-muted-foreground/50" />
-                    </span>
-                </div>
-                <div class="relative w-full items-center">
-                    <Input v-model="password" type="password" placeholder="Password" class="pl-10 py-5 rounded-xs" />
-                    <span class="absolute start-0 inset-y-0 flex items-center justify-center px-3">
-                        <Lock class="size-4 text-muted-foreground/50" />
-                    </span>
-                </div>
-                <div class="flex flex-col mt-4 gap-3">
-                    <Button class="rounded-xs py-5" @click="handleLogin">Login</Button>
-                    <Button variant="outline" class="rounded-xs py-5" @click="handleSignUp">Sign up</Button>
-                </div>
-                <TextDivider>or</TextDivider>
-                <Button variant="outline" class="relative rounded-xs py-5" @click="googleRedirect">
-                    <img class="absolute left-2" src="../assets/icons/google.svg" width="20" />
-                    Sign in with Google
-                </Button>
-            </div>
+            <LoginForm />
             <!-- Continue As Guest -->
             <div class="flex flex-col gap-3">
                 <small class="text-center">Want to try it out first?</small>
