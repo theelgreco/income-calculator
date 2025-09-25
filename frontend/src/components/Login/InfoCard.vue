bfec937105dbe3ec4eb36be4d7610f8e71e9f514
theelgreco <stelios.kakoliris@yahoo.com>
2025-09-25 22:04:17 +0100

    adds InfoCard component

---
+++ frontend/src/components/Login/InfoCard.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+interface Props {
+    title: string;
+    description: string;
+}
+
+defineProps<Props>();
+</script>
+
+<template>
+    <div class="relative p-10 rounded border bg-card overflow-hidden">
+        <h3 class="text-lg font-semibold">{{ title }}</h3>
+        <small class="line-clamp-2">{{ description }}</small>
+        <slot></slot>
+    </div>
+</template>
