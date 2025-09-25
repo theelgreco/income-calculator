1cb18deb58540a989569015d6f3dde5f4876fa3b
theelgreco <stelios.kakoliris@yahoo.com>
2025-09-25 22:04:51 +0100

    moves LoginView elements into separate components

--- frontend/src/components/Login/LoginForm.vue
+++ frontend/src/components/Login/LoginForm.vue
@@ -68,37 +68,48 @@ onMounted(() => {
 </script>
 
 <template>
-    <Form :formSchema="formSchema" :form="form" class="flex flex-col gap-4 p-15 md:shadow-xl bg-white rounded-xl" @submit.prevent="">
-        <FormFieldGroup field="email">
-            <FormField>
-                <FormFieldInput placeholder="Email" />
-                <FormFieldIcon :icon="Mail" />
-            </FormField>
-            <FormFieldError />
-        </FormFieldGroup>
-        <FormFieldGroup field="password">
-            <FormField>
-                <FormFieldInput type="password" placeholder="Password" />
-                <FormFieldIcon :icon="Lock" />
-            </FormField>
-            <FormFieldError />
-        </FormFieldGroup>
-        <FormFooter class="flex flex-col gap-6">
-            <div class="flex flex-col mt-4 gap-3">
-                <Button class="rounded-xs py-5" @click="handleLogin" type="submit" :disabled="form.submitting.value">Login</Button>
-                <Button variant="outline" class="rounded-xs py-5" @click="handleSignUp" type="submit" :disabled="form.submitting.value"
-                    >Sign up</Button
-                >
-            </div>
-            <TextDivider>or</TextDivider>
-            <Button variant="outline" class="relative rounded-xs py-5" @click="googleRedirect" :disabled="form.submitting.value">
-                <img class="absolute left-2" src="@/assets/icons/google.svg" width="20" />
-                Sign in with Google
-            </Button>
-            <div class="flex flex-col gap-3 mt-10">
+    <div>
+        <Form
+            :formSchema="formSchema"
+            :form="form"
+            class="flex flex-col gap-4 p-10 sm:p-15 md:shadow-xl bg-white rounded-xl"
+            @submit.prevent=""
+        >
+            <FormFieldGroup field="email">
+                <FormField>
+                    <FormFieldInput placeholder="Email" />
+                    <FormFieldIcon :icon="Mail" />
+                </FormField>
+                <FormFieldError />
+            </FormFieldGroup>
+            <FormFieldGroup field="password">
+                <FormField>
+                    <FormFieldInput type="password" placeholder="Password" />
+                    <FormFieldIcon :icon="Lock" />
+                </FormField>
+                <FormFieldError />
+            </FormFieldGroup>
+            <FormFooter class="flex flex-col gap-6">
+                <div class="flex flex-col mt-4 gap-3">
+                    <Button class="rounded-xs py-5" @click="handleLogin" type="submit" :disabled="form.submitting.value">Login</Button>
+                    <Button variant="outline" class="rounded-xs py-5" @click="handleSignUp" type="submit" :disabled="form.submitting.value"
+                        >Sign up</Button
+                    >
+                </div>
+                <TextDivider>or</TextDivider>
+                <Button variant="outline" class="relative rounded-xs py-5" @click="googleRedirect" :disabled="form.submitting.value">
+                    <img class="absolute left-2" src="@/assets/icons/google.svg" width="20" />
+                    Sign in with Google
+                </Button>
+                <!-- <div class="flex flex-col gap-3 mt-10">
                 <small class="text-center">Want to try it out first?</small>
                 <Button variant="secondary" size="lg" :disabled="form.submitting.value">Continue as guest</Button>
-            </div>
-        </FormFooter>
-    </Form>
+            </div> -->
+                <div class="flex flex-col items-center mt-10 mx-auto">
+                    <small class="text-center"> Want to try it out first? </small>
+                    <small class="text-violet-600">Continue as guest</small>
+                </div>
+            </FormFooter>
+        </Form>
+    </div>
 </template>
--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -1,23 +1,28 @@
 <script setup lang="ts">
 import LoginForm from "@/components/login/LoginForm.vue";
+import LoginInfo from "@/components/login/LoginInfo.vue";
+import { motion, type MotionProps } from "motion-v";
+
+const containerMotions: MotionProps = {
+    initial: {
+        backgroundImage:
+            "linear-gradient(180deg, rgba(255, 255, 255, 0) 0%, rgba(255, 255, 255, 0) 10%, rgba(255, 255, 255, 0) 90%, rgba(255, 255, 255, 0) 100%), radial-gradient(#000000 0px, #ffffff 0px)",
+    },
+    animate: {
+        backgroundImage:
+            "linear-gradient(180deg, rgba(255, 255, 255, 0) 0%, rgba(255, 255, 255, 1) 10%, rgba(255, 255, 255, 1) 90%, rgba(255, 255, 255, 0) 100%), radial-gradient(#000000 1px, #ffffff 1px)",
+    },
+    transition: {
+        duration: 1,
+    },
+};
 </script>
 
 <template>
-    <div class="h-full flex bg-gradient-to-tr from-yellow-50 to-white to-40%">
-        <div class="flex flex-col gap-5 max-lg:hidden w-1/2 my-60 mx-20">
-            <div class="flex flex-wrap items-center gap-5">
-                <img class="bg-white border rounded-full p-5 shadow-lg w-[100px] h-[100px]" src="/192w/icon.png" />
-                <div>
-                    <h1 class="font-black text-3xl">Take control of your finances today</h1>
-                    <h2 class="text-lg font-light">One <i class="font-bold">simple</i> transaction at a time</h2>
-                </div>
-            </div>
-            <!-- <div class="w-full border h-full"></div> -->
-        </div>
-        <div
-            class="md:bg-[#F8F8F8] md:border md:shadow-2xl flex flex-col justify-center w-full lg:w-1/2 sm:px-10 md:px-35 rounded-3xl md:min-w-[700px]"
-        >
-            <LoginForm />
-        </div>
-    </div>
+    <motion.div v-bind="containerMotions" class="h-full flex justify-between login-container [background-size:100%,19px_19px]">
+        <LoginInfo class="flex flex-col justify-center gap-5 max-lg:hidden w-1/2 h-full px-20" />
+        <LoginForm
+            class="md:bg-[#F8F8F8] md:border md:shadow-2xl flex flex-col justify-center w-full lg:w-1/2 sm:px-10 md:px-35 rounded-3xl md:min-w-[700px] max-w-[1000px]"
+        />
+    </motion.div>
 </template>
