df20487dd521b1c40173a5ab65fa6ef2fc88ed5c
theelgreco <stelios.kakoliris@yahoo.com>
2025-09-26 10:22:30 +0100

    improves bg animation

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -4,13 +4,12 @@ import LoginInfo from "@/components/login/LoginInfo.vue";
 import { motion, type MotionProps } from "motion-v";
 
 const containerMotions: MotionProps = {
-    initial: {
-        backgroundImage:
-            "linear-gradient(180deg, rgba(255, 255, 255, 0) 0%, rgba(255, 255, 255, 0) 10%, rgba(255, 255, 255, 0) 90%, rgba(255, 255, 255, 0) 100%), radial-gradient(#000000 0px, #ffffff 0px)",
-    },
     animate: {
-        backgroundImage:
+        backgroundImage: [
+            "linear-gradient(180deg, rgba(255, 255, 255, 0) 0%, rgba(255, 255, 255, 1) 10%, rgba(255, 255, 255, 1) 90%, rgba(255, 255, 255, 0) 100%), radial-gradient(#000000 0px, #ffffff 0px)",
+            "linear-gradient(180deg, rgba(255, 255, 255, 0) 0%, rgba(255, 255, 255, 1) 10%, rgba(255, 255, 255, 1) 90%, rgba(255, 255, 255, 0) 100%), radial-gradient(#000000 2px, #ffffff 2px)",
             "linear-gradient(180deg, rgba(255, 255, 255, 0) 0%, rgba(255, 255, 255, 1) 10%, rgba(255, 255, 255, 1) 90%, rgba(255, 255, 255, 0) 100%), radial-gradient(#000000 1px, #ffffff 1px)",
+        ],
     },
     transition: {
         duration: 1,
@@ -19,10 +18,24 @@ const containerMotions: MotionProps = {
 </script>
 
 <template>
-    <motion.div v-bind="containerMotions" class="h-full flex justify-between login-container [background-size:100%,19px_19px]">
+    <motion.div v-bind="containerMotions" class="h-full flex justify-between login-bg">
         <LoginInfo class="flex flex-col justify-center gap-5 max-lg:hidden w-1/2 h-full px-20" />
         <LoginForm
             class="md:bg-[#F8F8F8] md:border md:shadow-2xl flex flex-col justify-center w-full lg:w-1/2 sm:px-10 md:px-35 rounded-3xl md:min-w-[700px] max-w-[1000px]"
         />
     </motion.div>
 </template>
+
+<style scoped>
+.login-bg {
+    background-size: 100%, 19px 19px;
+    background-image: linear-gradient(
+            180deg,
+            rgba(255, 255, 255, 0) 0%,
+            rgba(255, 255, 255, 1) 10%,
+            rgba(255, 255, 255, 1) 90%,
+            rgba(255, 255, 255, 0) 100%
+        ),
+        radial-gradient(#000000 1px, #ffffff 1px);
+}
+</style>
