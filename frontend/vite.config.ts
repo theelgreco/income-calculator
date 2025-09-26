cd5daa92ce690e17e45d43b42c58bcdf32578880
theelgreco <stelios.kakoliris@yahoo.com>
2025-09-26 10:38:25 +0100

    adds auto importer

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,12 +4,14 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueDevTools from "vite-plugin-vue-devtools";
 import { VitePWA } from "vite-plugin-pwa";
+import Components from "unplugin-vue-components/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
         vue(),
         vueDevTools(),
+        Components(),
         VitePWA({
             registerType: "autoUpdate",
             manifest: {
