655e1895527ff67ff5908270c49080ecc7b68907
theelgreco <stelios.kakoliris@yahoo.com>
2025-09-26 17:18:59 +0100

    refactors LoginInfo animations

--- frontend/src/components/login/LoginInfo.vue
+++ frontend/src/components/login/LoginInfo.vue
@@ -1,13 +1,8 @@
 <script setup lang="ts">
-import { motion, type MotionProps } from "motion-v";
+import { motion } from "motion-v";
 import { CalendarIcon, CurrencyIcon, GlobeIcon } from "lucide-vue-next";
 import InfoCard from "@/components/login/InfoCard.vue";
-
-const headingMotions: MotionProps = {
-    initial: { y: 25, opacity: 0 },
-    animate: { y: 0, opacity: 1 },
-    transition: { type: "spring" },
-};
+import { fadeInFromBottom } from "@/lib/motions";
 
 const infoCards = [
     {
@@ -30,9 +25,8 @@ const infoCards = [
 
 <template>
     <div>
-        <div class="flex w-full flex-wrap gap-5 max-xl:justify-center max-xl:text-center">
+        <motion.div v-bind="fadeInFromBottom" class="flex w-full flex-wrap gap-5 max-xl:justify-center max-xl:text-center">
             <motion.img
-                v-bind="headingMotions"
                 :whilePress="{ scale: 0.9 }"
                 :drag="true"
                 :dragConstraints="{ top: 0, right: 0, bottom: 0, left: 0 }"
@@ -40,12 +34,10 @@ const infoCards = [
                 src="/192w/icon.png"
             />
             <div class="flex flex-col justify-center">
-                <motion.h1 v-bind="headingMotions" class="font-black text-lg xl:text-3xl">Take control of your finances today</motion.h1>
-                <motion.h2 v-bind="headingMotions" class="text-sm xl:text-lg font-light">
-                    One <i class="font-bold">simple</i> transaction at a time
-                </motion.h2>
+                <h1 class="font-black text-lg xl:text-3xl">Take control of your finances today</h1>
+                <h2 class="text-sm xl:text-lg font-light">One <i class="font-bold">simple</i> transaction at a time</h2>
             </div>
-        </div>
+        </motion.div>
         <div class="mt-5 xl:mt-10 w-full flex flex-col gap-3">
             <InfoCard v-for="(infoCard, index) in infoCards" :key="infoCard.title" :delayIndex="index" v-bind="infoCard" />
         </div>
