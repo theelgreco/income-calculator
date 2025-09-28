1c19e5879a796e3cea96fd2fd38b252caee38673
theelgreco <stelios.kakoliris@yahoo.com>
2025-09-28 16:41:10 +0100

    adds NODE_ENV values to global namespace

--- backend/src/types/environment.d.ts
+++ backend/src/types/environment.d.ts
@@ -1,7 +1,7 @@
 declare global {
     namespace NodeJS {
         interface ProcessEnv {
-            NODE_ENV: "development" | "production";
+            NODE_ENV: "production" | "staging" | "development";
             DATABASE_URL: string;
             JWT_KEY: string;
             PORT?: string;
