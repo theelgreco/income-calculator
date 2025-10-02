392350b6050fd7b2a49afb97ea4464ea3ee216d0
theelgreco <stelios.kakoliris@yahoo.com>
2025-10-02 22:24:46 +0100

    renames cors variable

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import type { Express, Request, Response } from "express";
 import dotenv from "dotenv";
-import e from "cors";
+import cors from "cors";
 import { authenticateJWT } from "./jwt/jwt";
 import { handleCustomErrors } from "./errors/middleware";
 import {
@@ -26,7 +26,7 @@ const port = Number(process.env.PORT) || 3000;
 const publicDir = path.resolve(__dirname, "..", "public");
 
 app.use(express.json());
-app.use(e());
+app.use(cors());
 app.use(express.static(publicDir));
 
 const swaggerUiOptions = {
