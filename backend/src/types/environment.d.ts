44750c3d766c55b3b9934d474801b6f81f16bec4
theelgreco <stelios.kakoliris@yahoo.com>
2025-10-12 10:21:19 +0100

    adds new environment variables to global type declaration

--- backend/src/types/environment.d.ts
+++ backend/src/types/environment.d.ts
@@ -4,5 +4,10 @@ declare namespace NodeJS {
         DATABASE_URL: string;
         JWT_KEY: string;
         PORT?: string;
+        EMAIL_HOST: string;
+        EMAIL_PORT: string;
+        EMAIL_SECURE: string;
+        EMAIL_USER: string;
+        EMAIL_PASSWORD: string;
     }
 }
