811a44c47cd4aa8b510c52d5004864d3ebbfc546
theelgreco <stelios.kakoliris@yahoo.com>
2025-10-13 23:53:26 +0100

    refactor: correct NOREPLY email address format and improve error handling in sendEmail function

--- backend/src/emails/index.ts
+++ backend/src/emails/index.ts
@@ -3,19 +3,25 @@ import { MailOptions } from "nodemailer/lib/json-transport";
 
 export const enum EmailSenders {
     BOSS = "stelios@fidelio.club",
-    NOREPLY = "noreply@fidelio.club",
+    NOREPLY = "no-reply@fidelio.club",
 }
 
 type EmailOptions = MailOptions & { from: EmailSenders };
 
 export async function sendEmail(emailOptions: EmailOptions) {
-    return await createTransport({
-        host: process.env.EMAIL_HOST,
-        port: parseInt(process.env.EMAIL_PORT as string),
-        secure: process.env.EMAIL_SECURE === "true",
-        auth: {
-            user: emailOptions.from,
-            pass: process.env.EMAIL_PASSWORD,
-        },
-    }).sendMail({ ...emailOptions });
+    try {
+        const transporter = createTransport({
+            host: process.env.EMAIL_HOST,
+            port: parseInt(process.env.EMAIL_PORT as string),
+            secure: process.env.EMAIL_SECURE === "true",
+            auth: {
+                user: emailOptions.from,
+                pass: process.env.EMAIL_PASSWORD,
+            },
+        });
+
+        return await transporter.sendMail({ ...emailOptions });
+    } catch (err: unknown) {
+        console.error(err);
+    }
 }
